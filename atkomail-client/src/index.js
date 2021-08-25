47f19ca409ec6ec0bd2414515a498a9ce70c4285
Andy March <mail@andymarch.co.uk>
2021-08-25 11:36:58 +0100

    Adding dynamic version

--- atkomail-client/src/index.js
+++ atkomail-client/src/index.js
@@ -9,14 +9,17 @@ import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
+import GitInfo from 'react-git-info/macro';
 TimeAgo.addDefaultLocale(en)
+
+const gitInfo = GitInfo();
 
 Sentry.init({
   dsn: "https://f65e6bc426164cb583bc42f2a9d4eed9@o876653.ingest.sentry.io/5890468",
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
   autoSessionTracking: true,
-  release: "atkomail@1.0",
+  release: gitInfo.commit.hash,
 });
 
 
